1d7f4667f1ab6a1f68cc0d60d8d4ea1f6d3e9858
cnegriuc <125255852+cnegriuc@users.noreply.github.com>
2023-04-05 16:25:10 +0300

    Update streamlit_app.py
    added the menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,8 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast')
+streamlit.text('Omlette')
+streamlit.text('English Breakfast')
+streamlit.text('American Breakfast')
+streamlit.header('Lunch')
+streamlit.text('Lentile soup')
